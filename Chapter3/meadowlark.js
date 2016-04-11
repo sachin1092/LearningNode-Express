635ff77392a5dc7e82bb586126cdccd82704112d
sachin1092 <me@sachinshinde.com>
2016-04-11 11:44:09 -0700

    moved the fortune functionality to module

--- Chapter3/meadowlark.js
+++ Chapter3/meadowlark.js
@@ -7,13 +7,7 @@ var handlebars = require('express3-handlebars')
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-var fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-];
+var fortune = require('./lib/fortune.js');
 
 app.set('port', process.env.PORT || 3000);
 
@@ -24,8 +18,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/about', function(req, res) {
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', {fortune: randomFortune});
+    res.render('about', {fortune: fortune.getFortune()});
 });
 
 app.use(function (req, res, next) {
