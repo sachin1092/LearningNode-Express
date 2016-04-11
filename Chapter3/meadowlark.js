66b187f0ac537ac2c8d6aa5dfda868e11e9f2577
sachin1092 <me@sachinshinde.com>
2016-04-11 00:44:16 -0700

    added handlebars

--- Chapter3/meadowlark.js
+++ Chapter3/meadowlark.js
@@ -2,28 +2,41 @@ var express = require('express');
 
 var app = express();
 
+var handlebars = require('express3-handlebars')
+        .create({defaultLayout: 'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+var fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+];
+
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', function(req, res) {
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
+    res.render('home');
 });
 
 app.get('/about', function(req, res) {
-    res.type('text/plain');
-    res.send('About Meadowlark Travel');
+    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', {fortune: randomFortune});
 });
 
-app.use(function (req, res) {
-    res.type('text/plain');
+app.use(function (req, res, next) {
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 
-app.use(function (req, res) {
-    res.type('text/plain');
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function() {
